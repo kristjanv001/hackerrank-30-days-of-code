2f5527c84de3ad1cee1b9a29d5664afd3536f289
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-02-22 13:39:46 +0200

    Create 11-2d-array.java

---
+++ 11-2d-array.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+           
+        scanner.close();
+        
+    
+        int result = hourGlass(arr);
+        System.out.println(result);
+        
+    }
+    
+    public static int hourGlass(int[][] arr) {
+        
+        int rows = arr.length;
+        int cols = arr[0].length;
+        
+        int max = 0;
+
+        
+        for (int i = 0; i < arr.length-2; i++) { 
+            for (int j = 0; j < arr[0].length-2; j++) {
+                int chunkMax = 0;
+                
+                chunkMax = 
+                    // row 1 cols 1-3
+                    arr[i][j] +
+                    arr[i][j+1] +
+                    arr[i][j+2] +
+                    // row 2 col 2
+                    arr[i+1][j+1] +
+                    // row 3 col 1-3
+                    arr[i+2][j] +
+                    arr[i+2][j+1] +
+                    arr[i+2][j+2]
+                ;
+                
+                if (chunkMax > max) {
+                    max = chunkMax;        
+                }
+            }
+
+        }
+        return max;
+    }
+        
+
+}
