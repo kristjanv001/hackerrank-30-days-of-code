437da95a347e84fa7e306bed272f4e1d8d4a7c4d
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-02-25 08:42:34 +0200

    Create 14-scope.java

---
+++ 14-scope.java
@@ -0,0 +1,15 @@
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+	// Add your code here
+    Difference(int[] els) {
+        this.elements = els;
+    }
+     
+    void computeDifference() {
+        Arrays.sort(elements);
+        maximumDifference = Math.abs(elements[elements.length-1] - elements[0]);
+    }
+
+}
