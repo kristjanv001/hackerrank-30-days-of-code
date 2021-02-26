e867b0643b6fe3c53e9c56e7d771d33a13772807
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-02-26 08:00:14 +0200

    Create 16-exceptions.java

---
+++ 16-exceptions.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        
+        try {
+            System.out.println(Integer.parseInt(S));
+        } catch(NumberFormatException e) {
+            System.out.println("Bad String");
+        }
+    }
+}
