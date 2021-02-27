7b72190e728436a520798ffee0b33e087bcdad4e
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-02-27 11:43:50 +0200

    Update 18-stacks-queues.java

--- 18-stacks-queues.java
+++ 18-stacks-queues.java
@@ -1,12 +1,17 @@
-    Stack<Character> stack = new Stack<>();
+public class Solution {
+    // Write your code here.
+    
+    // https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
+    // Stack<Character> stack = new Stack<>();
+    LinkedList<Character> stack = new LinkedList<>();
     LinkedList<Character> queue = new LinkedList<>();
     
     public void pushCharacter(char ch) {
-        stack.push(ch);
+        stack.addFirst(ch);
     }
     
     public char popCharacter() {
-        return stack.pop();
+        return stack.removeFirst();
     }
     
     public void enqueueCharacter(char ch) {
@@ -15,4 +20,8 @@
     
     public char dequeueCharacter() {
         return queue.remove();
-    }
+    } 
+    
+    
+// I used the Stack class at first, but then read from the docs that it's better to use the Deque class, 
+// but since it's an abstract class and can't be intantiated I decided to just use the LinkedList class
