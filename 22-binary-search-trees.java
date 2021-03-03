20cc81c34226e570a294b717db8f359c354fee36
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-03-03 09:20:11 +0200

    Create 22-binary-search-trees.java

---
+++ 22-binary-search-trees.java
@@ -0,0 +1,9 @@
+	public static int getHeight(Node root){
+      //Write your code here
+      if (root == null) {
+          return -1;
+      } else {
+          return Math.max(getHeight(root.left), getHeight(root.right))+1;
+      }
+    }
+
