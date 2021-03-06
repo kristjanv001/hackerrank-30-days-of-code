2b538464e7658675cceb0b6c34f500e0b7d199df
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-03-06 11:08:20 +0200

    Create 27-testing.java

---
+++ 27-testing.java
@@ -0,0 +1,34 @@
+    static class TestDataEmptyArray {
+        public static int[] get_array() {
+            // complete this function
+            int[] emptyArr = new int[0];
+            return emptyArr;
+        }
+    }
+
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            // complete this function
+            int[] arr = {2, 4};
+            return arr;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return 0;
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+            // complete this function
+            int[] arr = {1, 1};
+            return arr;
+            
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return 0;
+        }
+    }
